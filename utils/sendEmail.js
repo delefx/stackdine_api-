f36dac482201a459fc63fcd544e5024a5f303cfc
dele5x <omodaraayodele7@gmail.com>
2026-06-02 14:16:21 +0100

    aa

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,16 +1,32 @@
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
+
+// const sendEmail = async ({ to, subject, html }) => {
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: `"StackDine" <${process.env.EMAIL_USER}>`,
+//     to,
+//     subject,
+//     html,
+//   });
+// };
+
+// module.exports = sendEmail;
+
+const { Resend } = require('resend');
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  // Initialize Resend inside the function or globally using your new env key
+  const resend = new Resend(process.env.RESEND_API_KEY);
 
-  await transporter.sendMail({
-    from: `"StackDine" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: 'StackDine <onboarding@resend.dev>', // Resend's default free domain identifier
     to,
     subject,
     html,
